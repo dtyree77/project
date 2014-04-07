98b0ead14612d501272162b873b2ee6a2d7fc229
David Tyree <dtyree@fox.local>
2014-04-07 15:18:48 -0600

    Updated the test script

--- test.sh
+++ test.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+rm -rf coverage
+
 # Start our service with code coverage
 ./node_modules/.bin/istanbul cover service.js& 
 
-NODE_PID=$!
-
-sleep 1 
+sleep 2 
 
 # Run our unit tests, this works the same way as running them from a browser
 ./node_modules/.bin/hopjs --url http://localhost:3000/api/ --unitTest
 
-if [ -x .coveralls.yml ]
+# Tell our service to exit nicely
+./node_modules/.bin/hopjs --url http://localhost:3000/api/ TestService.exit
+
+
+# Submit the code coverage data if we're using coveralls
+if [ -f .coveralls.yml ]
 then
-  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js 
-  rm -rf ./coverage
+  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js  &
 fi
 
-kill $NODE_PID
